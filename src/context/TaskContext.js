d9555350c11daa9cc45d4d1e168824d8fa1bef46
avan0605 <avansharmaabd@gmail.com>
2026-02-18 15:05:07 +0530

    Add global task context

---
+++ src/context/TaskContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const TaskContext = createContext();
+
+export const TaskProvider = ({ children }) => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  return (
+    <TaskContext.Provider value={{ tasks, addTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
